0619c79d7b435cbf8098ea2ba73410d26b6442f4
Chris Robinson <chrobins@ucsd.edu>
2020-02-27 23:32:50 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = '8Ebj16AvK2R51XKEMtvRJixnbvpMWaSO';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+  		center: [32.878848, -117.235869],
+  		layers: L.mapquest.tileLayer('map'),
+  		zoom: 12,
+  		zoomControl: false
+	});
+
+	L.marker([32.878848, -117.235869]).addTo(map);
 }
